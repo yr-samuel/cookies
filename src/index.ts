39e21f8202842bec3b102ffa348ada9656e71d8e
Samuel Oliveira <samuel.aoliveira74@gmail.com>
2024-10-15 00:27:03 -0300

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+
+app.get("/index", (_, response: Response) => {
+    response.cookie('cookie', 'cookie', {
+        expires: new Date(Date.now() + 900_000),
+        domain: 'localhost',
+        path: '/',
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    });
+    
+    response.json({ data: 1});
+});
+
+app.get('/hc', (_, response) => {
+    response.json({ is: 'working'})
+})
+
+app.listen(8000, () => {    
+    console.log("tamo rodando na porta 8080")
+});
